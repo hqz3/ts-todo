7112d03332ed0e65f7bc4450630c08b6286ed375
hqz3 <heqzheng@gmail.com>
2023-01-12 22:22:29 -0500

    Create app initializer

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
 import "./css/style.css";
+import ListTemplate from "./templates/ListTemplate";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent): void => {
+    e.preventDefault();
+    const form = e.currentTarget as HTMLFormElement;
+    const input = form.querySelector("input") as HTMLInputElement;
+
+    const newEntryText = input.value.trim();
+    if (!newEntryText) return;
+
+    const newListItem = new ListItem(Date.now().toString(), newEntryText);
+    fullList.addItem(newListItem);
+    template.render(fullList);
+    input.value = "";
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
